7a6762c171cceabf0f844173685a9991e5caf604
Iwan Moreton <me@iwanmoreton.com>
2020-11-04 09:51:26 +0000

    remove hardcoded secretkey

--- apps/todo/application/__init__.py
+++ apps/todo/application/__init__.py
@@ -5,7 +5,7 @@ from os import getenv
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI_TODO')
-app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+app.config['SECRET_KEY'] = getenv('TODO_SECRET_KEY')
 
 db = SQLAlchemy(app)
 from application import routes, forms
